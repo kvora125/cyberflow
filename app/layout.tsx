91495002963b7e39263d412c4e84f52e42568105
Keval Vora <41047220+kvora125@users.noreply.github.com>
2024-11-06 19:42:16 +0530

    meta data added

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,11 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "CyberFlow",
-  description: "A new stack overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
